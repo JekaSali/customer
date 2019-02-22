454a8dcbf9e0e6241d7080ce6f979ab2c0974570
Jekaterina Salina <jekaterinasalina@gmail.com>
2019-02-22 15:59:17 +0200

    update

---
+++ src/main/java/io/codelex/customer/CustomerRepository.java
@@ -0,0 +1,9 @@
+package io.codelex.customer;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+}
